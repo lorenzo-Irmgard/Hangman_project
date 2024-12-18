743432fec001b3bae7fff2922e467da9b80141d6
Vadim <beregov_92@mail.ru>
2024-12-18 15:44:45 +0300

    Word pick feature added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 package src;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.List;
 import java.io.File;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    static String path = "src/Resources/words.txt";
+    static int errcode = 0;
+
+    public static void main(String[] args) throws FileNotFoundException {
         int state = 0;
         String userAnswer;
         Scanner menu = new Scanner(System.in);
-        while (state == 0) {
+        while (state == 0 && errcode == 0) {
             System.out.println("Type N for starting a new game or E for exit");
             switch (userAnswer = menu.nextLine()) {
                 case "E":
@@ -31,13 +35,25 @@ public class Main {
         }
     }
 
-    public static void gameCycle() {
+    public static void gameCycle() throws FileNotFoundException {
         List <Character> validChars = new ArrayList<>();
         List <Character> invalidChars = new ArrayList<>();
-        String word =
+        String word = wordsBank(path);
+        System.out.println(word);N
     }
 
-    public static String wordsBank(String filepath) {
-
+    public static String wordsBank(String filepath) throws FileNotFoundException {
+        String word;
+        Random rand = new Random();
+        List <String> allWords = new ArrayList<>();
+        File file = new File(filepath);
+        if (file == null) return ""; //TODO Добавить обработку краевых ситуаций и выброс ошибок если файла не существует
+        Scanner scanWords = new Scanner(file);//TODO добавить слова в банк слов
+        while (scanWords.hasNextLine()) {
+            allWords.add(scanWords.nextLine().trim());
+        }
+        word = allWords.get(rand.nextInt(allWords.size()));
+        scanWords.close();
+        return word;
     }
 }
